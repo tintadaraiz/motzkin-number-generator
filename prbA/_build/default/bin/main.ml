35d708523544f224b5cdba34b2bc33678d09d1ee
rodrigo <rd810silva@gmail.com>
2023-03-27 09:41:47 +0100

    update

--- prbA/_build/default/bin/main.ml
+++ prbA/_build/default/bin/main.ml
@@ -14,34 +14,33 @@
 (* https://www.cs.mcgill.ca/~abeaul10/blog/2.html   <- última inspiração                                                                    *)
 (********************************************************************************************************************************************)
 
-(* biblioteca(s) *)
 
 (* programa *)
 (* input (utilizador) *)
-let num = Scanf.scanf "%d" (fun a -> a)
+let num = read_int ()
 
 (* criação da HashTable *)
-let memo = Hashtbl.create 10000
+let memo = Hashtbl.create (num+1)
 let () = Hashtbl.add memo 0 Z.one
 let () = Hashtbl.add memo 1 Z.one
 
-
 (* memoization *)
 let rec memoized_mot num =
-    match Hashtbl.mem memo num with
-    | true -> Hashtbl.find memo num
-    | false ->
-      let op1 num = (2 * num) + 1  in (* funções auxiliares *)
-      let op2 num = (3 * num) - 3 in
-      let op3 num = num + 2 in
-      let result = Z.div (Z.add (Z.mul (Z.of_int (op1 num)) (memoized_mot (num-1))) (Z.mul (Z.of_int (op2 num)) (memoized_mot (num-2)))) (Z.of_int (op3 num)) in
-      Hashtbl.add memo num result;
-      result
+  if (num >= 0 && num <= 10000) then
+      match Hashtbl.mem memo num with
+      | true -> Hashtbl.find memo num
+      | false ->
+        let op1 num = (2 * num) + 1  in (* funções auxiliares *)
+        let op2 num = (3 * num) - 3 in
+        let op3 num = num + 2 in
+        let result = Z.div (Z.add (Z.mul (Z.of_int (op1 num)) (memoized_mot (num-1))) (Z.mul (Z.of_int (op2 num)) (memoized_mot (num-2)))) (Z.of_int (op3 num)) in
+        Hashtbl.add memo num result;
+        result
+    else invalid_arg " o número introduzido tem de estar no intervalo 0 <= num <= 10000 "
 
-(* verificação do input && output *)
-let () = 
-  if (num >= 0 && num <= 10000) then num |> memoized_mot |> Z.print; 
-  print_endline ("")
+
+(* num -> memoized_mot -> Z.print -> endline *)
+let () = num |> memoized_mot |> Z.print; print_endline ("")
 
 
 (********************************************************************************************************************************************)
